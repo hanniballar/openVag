449f2c81b5337c6d6a2faa0d3846d03e92a203fb
Septimiu <hanniballar@yahoo.com>
2024-12-08 12:16:31 +0200

    Icon set on window

--- openVag/openVagGui/OpenVag.cpp
+++ openVag/openVagGui/OpenVag.cpp
@@ -318,9 +318,21 @@ bool OpenVag::Create()
 {
     // Create application window
     //ImGui_ImplWin32_EnableDpiAwareness();
-    WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, L"ImGui Example", nullptr };
+    WNDCLASSEXW wc = { 
+        sizeof(wc), 
+        CS_CLASSDC, 
+        WndProc, 
+        0L, 
+        0L, 
+        GetModuleHandle(nullptr), 
+        LoadIcon(GetModuleHandle(nullptr), IDI_APPLICATION), 
+        nullptr, 
+        nullptr, 
+        nullptr, 
+        L"ImGui Example",
+        LoadIcon(GetModuleHandle(nullptr), IDI_APPLICATION) };
     ::RegisterClassExW(&wc);
-    HWND hwnd = ::CreateWindowW(wc.lpszClassName, L"OpenVag", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, nullptr, nullptr, wc.hInstance, nullptr);
+    HWND hwnd = ::CreateWindowW(wc.lpszClassName, L"openVag", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, nullptr, nullptr, wc.hInstance, nullptr);
 
     // Initialize Direct3D
     if (!CreateDeviceD3D(hwnd))
--- openVag/openVagGui/main.cpp
+++ openVag/openVagGui/main.cpp
@@ -1,8 +1,28 @@
 #include "OpenVag.h"
 
-int main(int, char**)
-{
+# if defined(_WIN32)
+#     define NOMINMAX
+#     define WIN32_LEAN_AND_MEAN
+#     include <windows.h>
+#     include <stdlib.h> // __argc, argv
+# endif
+
+int Main(int argc, char** argv) {
     OpenVag openVag;
     if (openVag.Create())
         openVag.Run();
+    return 0;
 }
+
+# if defined(_WIN32) && !defined(_CONSOLE)
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+    return Main(__argc, __argv);
+}
+# else
+int main(int, char**)
+{
+    return Main(argc, argv);
+}
+# endif
+
